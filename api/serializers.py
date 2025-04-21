d797c945885e267f1646eb9cee8379ae18e41d82
Max Nölp <noelpmax@gmail.com>
2025-04-21 19:34:19 +0200

    extends serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Todo, Skill, Projects
+from filer.models import Image
 
 
 class TodoSerializer(serializers.ModelSerializer):
@@ -11,10 +12,18 @@ class TodoSerializer(serializers.ModelSerializer):
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
+        fields = ["id", "url"]
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
         fields = "__all__"
 
 
 class ProjectsSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Projects
         fields = "__all__"
