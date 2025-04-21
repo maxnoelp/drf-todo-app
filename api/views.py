0d5d378ad0132224f39165587b3f0a0a492cdbc4
Max Nölp <noelpmax@gmail.com>
2025-04-21 19:24:57 +0200

    fix view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .serializers import TodoSerializer
-from .models import Todo
+from .serializers import TodoSerializer, SkillSerializer, ProjectsSerializer
+from .models import Todo, Projects, Skill
 from rest_framework.permissions import IsAuthenticated
 from .permission import ReadOnly
 
@@ -12,12 +12,12 @@ class TodoView(viewsets.ModelViewSet):
 
 
 class SkillView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
+    serializer_class = SkillSerializer
+    queryset = Skill.objects.all()
     permission_classes = [ReadOnly]
 
 
 class ProjectsView(viewsets.ModelViewSet):
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
+    serializer_class = ProjectsSerializer
+    queryset = Projects.objects.all()
     permission_classes = [ReadOnly]
