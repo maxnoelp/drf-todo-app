9f7e113a79267156d471dd8be871d4b1a2569465
Max Nölp <noelpmax@gmail.com>
2025-04-21 17:55:10 +0200

    add default storage

--- todo_app_api/settings.py
+++ todo_app_api/settings.py
@@ -181,6 +181,7 @@ STORAGES = {
 }
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
